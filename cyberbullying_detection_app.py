c815588d13164e4d5cc52c45c357a8a51ee13b1b
shashank2325 <55526290+shashank2325@users.noreply.github.com>
2023-11-27 18:14:17 -0800

    Update cyberbullying_detection_app.py

--- cyberbullying_detection_app.py
+++ cyberbullying_detection_app.py
@@ -7,15 +7,21 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 # Load your pre-trained XGBoost model
 model = joblib.load('cyber_bullying_XGB.pkl')
 
+# Load your dataset (replace 'dataset.csv' with the actual file path)
+dataset = pd.read_csv('cyberbullying_tweets.csv')  # Replace with your dataset file path
+
+# Extract the tweet_text column as training data
+training_data = dataset['tweet_text'].tolist()
+
 # Define a TF-IDF vectorizer
 tfidf_vectorizer = TfidfVectorizer()
 
 # Define the preprocess_input function
 def preprocess_input(user_input):
     # Fit and transform the TF-IDF vectorizer on your training data
-    tfidf_vectorizer.fit(training_data)  # Replace 'training_data' with your actual training data
-    tfidf_matrix = tfidf_vectorizer.transform([user_input])
-    return tfidf_matrix
+    tfidf_vectorizer.fit(training_data)
+    user_input_vectorized = tfidf_vectorizer.transform([user_input])
+    return user_input_vectorized
 
 # Define the Streamlit app
 st.title("Cyberbullying Detection App")
@@ -35,7 +41,7 @@ if st.button("Predict"):
     prediction = model.predict(dmatrix)[0]  # Get the prediction result (assuming binary classification)
 
     # Display the prediction result to the user
-    if prediction == 3:
+    if prediction == 1:
         st.write("Cyberbullying Detected")
     else:
         st.write("No Cyberbullying Detected")
