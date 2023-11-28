e868a19e0870ed0a0575a86c79422e9afab3a9eb
shashank2325 <55526290+shashank2325@users.noreply.github.com>
2023-11-27 18:19:20 -0800

    Update cyberbullying_detection_app.py

--- cyberbullying_detection_app.py
+++ cyberbullying_detection_app.py
@@ -34,11 +34,19 @@ if st.button("Predict"):
     # Preprocess the user input
     preprocessed_input = preprocess_input(user_input)
     
-    # Perform the prediction
-    prediction = model.predict(preprocessed_input)[0]  # Get the prediction result (assuming binary classification)
+    # Perform the prediction for multi-class classification
+    prediction = model.predict(preprocessed_input)  # Get the prediction result
 
-    # Display the prediction result to the user
-    if prediction == 1:
-        st.write("Cyberbullying Detected")
-    else:
-        st.write("No Cyberbullying Detected")
+    # Map the predicted class labels to class names
+    class_names = {
+        0: 'Age',
+        1: 'Ethnicity',
+        2: 'Gender',
+        3: 'Not Cyberbullying',
+        4: 'Other Cyberbullying',
+        5: 'Religion'
+    }
+
+    # Display the predicted class name based on the prediction
+    predicted_class = class_names.get(prediction[0], "Unknown")
+    st.write("Predicted Cyberbullying Type:", predicted_class)
